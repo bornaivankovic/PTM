cd4b14d3dcd79876deb52d274ef216617e770e4a
Dinko[cURL] <dinkochess@gmail.com>
2017-05-15 15:45:17 +0200

    neke stvari dodane

--- PTM/client/controllers/draw.ts
+++ PTM/client/controllers/draw.ts
@@ -1,3 +1,27 @@
 export class DrawController {
-    constructor() {};
+
+
+    constructor() {
+    };
+
+    
+
+    editNode(data:any, callback:any): void {
+      (<HTMLInputElement>document.getElementById('node-label')).value = data.label;
+      document.getElementById('node-saveButton').onclick = this.saveNodeData.bind(this, data, callback);
+      document.getElementById('node-cancelButton').onclick = this.clearNodePopUp.bind(this, data, callback);
+      document.getElementById('node-popUp').style.display = 'block';
+    }
+
+    clearNodePopUp(): void {
+        document.getElementById('node-saveButton').onclick = null;
+        document.getElementById('node-cancelButton').onclick = null;
+        document.getElementById('node-popUp').style.display = 'none';
+    }
+
+    saveNodeData(data:any,callback:any) {
+      data.label = (<HTMLInputElement>document.getElementById('node-label')).value;
+      this.clearNodePopUp();
+      callback(data);
+    }
 }
