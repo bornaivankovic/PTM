e2e7ef26b10b04f4c386cdc4e1f889ba0865aca0
Dinko[cURL] <dinkochess@gmail.com>
2017-05-15 13:53:59 +0200

    models

--- PTM/client/models/edge.ts
+++ PTM/client/models/edge.ts
@@ -1,13 +1,46 @@
 import { FailRepairRate } from './rates';
 
 export class Edge extends FailRepairRate {
+    
+    /*visualization*/
     private label: string;
     private id: string;
     private from: string;
     private to: string;
+    private length: number;
+
+    /*backend parameters*/
+    private src: string;
+    private dest: string;
+    
 
 
     /* Getters and setters */
+
+    getSrc(): string {
+        return this.src;
+    }
+
+    setSrc(src: string): void {
+        this.src = src;
+    }
+
+    getDest(): string {
+        return this.dest;
+    }
+
+    setDest(dest: string): void {
+        this.dest = dest;
+    }
+
+    getLength(): number {
+        return this.length;
+    }
+
+    setLength(length: number): void {
+        this.length = length;
+    }
+
 
     getLabel(): string {
         return this.label;
