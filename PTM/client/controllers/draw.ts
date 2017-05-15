5b026e1dbb838fba4befa5a7ce46fb60d6417874
Vedran Serenčeš <vedranserences@gmail.com>
2017-05-15 17:43:10 +0200

    Add

--- PTM/client/controllers/draw.ts
+++ PTM/client/controllers/draw.ts
@@ -1,15 +1,46 @@
 export class DrawController {
 
-    private data: any;
-    private enabled: boolean = true;
-   
-    constructor(data: any) {
-        this.data = data;
+    constructor() {
     };
 
-    addEdge(data: any, callback:any) {
-
+    public addNode(data: any, callback: any) {
+        // filling in the popup DOM elements
+        console.log("add node");
+        document.getElementById('node-operation').innerHTML = "Add Node";
+        this.editNode(data, callback);
+        
     }
 
+    addEdge(data: any, callback: any) {
+        document.getElementById('node-operation').innerHTML = "Add Node";
+        this.editNode(data, callback);
+    }
+
+    editNode(data: any, callback: any) {
+        console.log("tu sam");
+        (<HTMLInputElement>document.getElementById('node-label')).value = data.label;
+        document.getElementById('node-saveButton').onclick = this.saveNodeData.bind(this, data, callback);
+        //document.getElementById('node-cancelButton').onclick = this.clearNodePopUp(this);
+        document.getElementById('node-popUp').style.display = 'block';
+    }
+
+    clearNodePopUp() {
+        document.getElementById('node-saveButton').onclick = null;
+        document.getElementById('node-cancelButton').onclick = null;
+        document.getElementById('node-popUp').style.display = 'none';
+    }
+
+    cancelNodeEdit(callback: any) {
+        this.clearNodePopUp();
+        callback(null);
+    }
+
+    saveNodeData(data: any, callback: any) {
+        console.log("save");
+        data.label = (<HTMLInputElement>document.getElementById('node-label')).value;
+        this.clearNodePopUp();
+        data.label = 'nesto';
+        callback(data);
+    }
 
 }
--- PTM/client/main.ts
+++ PTM/client/main.ts
@@ -3,14 +3,16 @@ import { Edge } from './models/edge';
 import { DrawController } from './controllers/draw';
 
 declare var vis: any;
-function renderTopology(){
+
+let nodes: Node[] = new Array<Node>();
+let edges: Edge[] = new Array<Edge>();
+function renderTopology() {
     var container = document.getElementById('network');
     console.log(container);
-    
-    let nodes : Node[] = new Array<Node>();
-    let edges: Edge[] = new Array<Edge>();
 
-    
+
+
+
 
 
     let testnode: Node = new Node();
@@ -41,12 +43,101 @@ function renderTopology(){
     var draw = new DrawController();
 
     var options = {
-        manipulation:  draw
-
+        layout: {
+            randomSeed: 2
+        },
+        manipulation: {
+            addNode: function (data: any, callback: any) {
+                // filling in the popup DOM elements
+                document.getElementById('node-operation').innerHTML = "Add Node";
+                console.log("1");
+                editNode(data, callback);
+            },
+            editNode: function (data: any, callback: any) {
+                // filling in the popup DOM elements
+                document.getElementById('node-operation').innerHTML = "Edit Node";
+                editNode(data, callback);
+            },
+            addEdge: function (data: any, callback: any) {
+                if (data.from == data.to) {
+                    var r = confirm("Do you want to connect the node to itself?");
+                    if (r != true) {
+                        callback(null);
+                        return;
+                    }
+                }
+                document.getElementById('edge-operation').innerHTML = "Add Edge";
+                editEdgeWithoutDrag(data, callback);
+            },
+            editEdge: {
+                editWithoutDrag: function (data: any, callback: any) {
+                    document.getElementById('edge-operation').innerHTML = "Edit Edge";
+                    editEdgeWithoutDrag(data, callback);
+                }
+            }
+        }
     };
 
     // initialize your network!
     var network = new vis.Network(container, data, options);
+}
+
+function editNode(data: any, callback: any) {
+    (<HTMLInputElement>document.getElementById('node-label')).value = data.label;
+    document.getElementById('node-saveButton').onclick = saveNodeData.bind(this, data, callback);
+    document.getElementById('node-cancelButton').onclick = clearNodePopUp.bind(this);
+    document.getElementById('node-popUp').style.display = 'block';
+}
+
+function clearNodePopUp() {
+    document.getElementById('node-saveButton').onclick = null;
+    document.getElementById('node-cancelButton').onclick = null;
+    document.getElementById('node-popUp').style.display = 'none';
+}
+
+function cancelNodeEdit(callback: any) {
+    clearNodePopUp();
+    callback(null);
+}
+
+function saveNodeData(data: any, callback: any) {
+    data.label = (<HTMLInputElement>document.getElementById('node-label')).value;
+    clearNodePopUp();
+        nodes.push(data);
+            console.log(nodes);
+    callback(data);
+
+
+}
+
+function editEdgeWithoutDrag(data: any, callback: any) {
+    // filling in the popup DOM elements
+    (<HTMLInputElement>document.getElementById('edge-label')).value = data.label;
+    document.getElementById('edge-saveButton').onclick = saveEdgeData.bind(this, data, callback);
+    document.getElementById('edge-cancelButton').onclick = cancelEdgeEdit.bind(this, callback);
+    document.getElementById('edge-popUp').style.display = 'block';
+}
+
+function clearEdgePopUp() {
+    document.getElementById('edge-saveButton').onclick = null;
+    document.getElementById('edge-cancelButton').onclick = null;
+    document.getElementById('edge-popUp').style.display = 'none';
+}
+
+function cancelEdgeEdit(callback: any) {
+    clearEdgePopUp();
+    callback(null);
+}
+
+function saveEdgeData(data: any, callback: any) {
+    if (typeof data.to === 'object')
+        data.to = data.to.id
+    if (typeof data.from === 'object')
+        data.from = data.from.id
+    data.label = (<HTMLInputElement>document.getElementById('edge-label')).value;
+    clearEdgePopUp();
+    callback(data);
+
 }
 
 renderTopology();
